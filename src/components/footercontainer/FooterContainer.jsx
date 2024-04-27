23dd335b4f78687bf6a8c6630a0457451b2fc038
Chanchal0108 <145567321+Chanchal0108@users.noreply.github.com>
2024-04-27 14:56:47 +0530

    footer

--- src/components/footercontainer/FooterContainer.jsx
+++ src/components/footercontainer/FooterContainer.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './footercontainer.css';
-
+import img1 from '../../assests/ima.jpeg'
 const FooterContainer = () => {
   return (
-    <div >
+    <div  className='container'>
       
-      <section class="firstsection">
-        <div class="leftsection">
-            Get Access To Our <span class="name">Free Batches Now</span>
-            
-           
+      <section className="firstsection">
+        <div className="leftsection">
+            Get Access To Our <div ><spam className="name">Free Batches</spam> <spam>Now.</spam> </div> 
+          
+            <p className='para'>    Get instant access to high-quality material </p>
+
         </div>
-        <div class="rightsection">
-            <img src="" alt="image"/>
+       
+        <div className="rightsection">
+            <img src={img1} alt="image"/>
         </div>
     </section>
 
