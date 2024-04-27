fddadde876574e6bbda3ea0dfd3d4f97e88e7cc6
Chanchal0108 <145567321+Chanchal0108@users.noreply.github.com>
2024-04-27 15:28:43 +0530

    g

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import FooterContainer from './components/footercontainer/FooterContainer'
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import {Home,Aboutus, Course, Contactus} from './pages';
 
->>>>>>> e565761dcf3ac168622aea66710b980c81d5feb5
-
-import image from './assests/ima.jpeg';
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div >
-
-   <FooterContainer/>
-
-    </div>
-  )
-=======
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -30,7 +18,6 @@ const App = () => {
       </Routes>
     </Router>
   );
->>>>>>> e565761dcf3ac168622aea66710b980c81d5feb5
 }
 
 export default App
