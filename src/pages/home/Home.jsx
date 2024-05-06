c6bd629174963bde4bbfad689d17f929460cbf93
Dipu Kumar Singh <dipudbg44@gmail.com>
2024-05-07 02:01:21 +0530

    s

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './home.css';
 import Navbar from '../../components/navbar/Navbar';
+import Header from '../home/Header'
+import himg1 from '../../assests/Becomıng a Web developer 12.png'
+import himg2 from '../../assests/Dark Navy Blue Game Youtube Thumbnail 4.png'
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
+  
+
+      <div className='Home-ec'>
+      <h1>Enroll in Our Courses</h1>
+      </div>
+      <div className='hcourse-details'>
+      <img src={himg1} />
+      <img src={himg2} />
+      <img src={himg1} />
+      <img src={himg2} />
+      <img src={himg1} />
+      <img src={himg2} />
+      </div>
     </div>
   )
 }
